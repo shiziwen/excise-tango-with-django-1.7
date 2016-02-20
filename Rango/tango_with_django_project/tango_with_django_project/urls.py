6af89a96606293a33ebf60172928a7662c8e048f
shiziwen <shiziwen@gmail.com>
2016-02-20 17:39:41 +0800

    added static media

--- Rango/tango_with_django_project/tango_with_django_project/urls.py
+++ Rango/tango_with_django_project/tango_with_django_project/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rango import views
 
 urlpatterns = patterns('',
@@ -11,3 +14,13 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
