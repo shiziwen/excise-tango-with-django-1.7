5dd25bcff138bef3435bfa1d696019d4241037d6
shiziwen <shiziwen@gmail.com>
2016-02-20 21:41:09 +0800

    added models

--- Rango/tango_with_django_project/tango_with_django_project/settings.py
+++ Rango/tango_with_django_project/tango_with_django_project/settings.py
@@ -83,7 +83,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
